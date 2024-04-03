1a54f2f6ec1a2be40963c78ae4a5439c6a647ffa
lvl0_module2 <zoey.lestyk@gmail.com>
2024-04-03 12:04:38 -0700

    Day 3, I learned more

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,7 +9,11 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+		 String adjective = JOptionPane.showInputDialog("Enter a adjective. :]");
+		 String liquid = JOptionPane.showInputDialog("Enter a type of liquid. :}");
+		 String bodyPart = JOptionPane.showInputDialog("Enter a body part. :']");
+		 String verb = JOptionPane.showInputDialog("Enter a verb. :'}");
+		 String place = JOptionPane.showInputDialog("Enter a place. (':");
 		// Get the player to enter a type of liquid
 
 		// Get the player to enter a body part
@@ -23,12 +27,11 @@ public class MadlibsOfTheAmazon {
 		// the values entered by the player.
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
-		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		JOptionPane.showMessageDialog (null, "Piranhas are more " + adjective + " during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh " + liquid + " and will most\n"  +
+		"likely take a bite out of your " + bodyPart + " if you " + verb +". Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the " + place + ". Good luck!");
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,9 +1,34 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
+		int score = 0;
+		String answer1 = JOptionPane.showInputDialog("What's the biggest species of shark?");
+		if (answer1.equals("whale shark")) {
+			score = score + 1;
+		}
+		String answer2 = JOptionPane.showInputDialog("What's a baby shark called?");
+		if (answer2.equals("a pup")) {
+			score = score + 1;
+		}
+		String answer3 = JOptionPane.showInputDialog("What's the only species of shark that can live in fresh water?");
+		if (answer3.equals("bull shark")) {
+			score = score + 1;
+		}
+		String answer4 = JOptionPane.showInputDialog("How many rows of teeth do most sharks have?");
+		if (answer4.equals("five")) { 
+			score = score + 1;
+		}
+		String answer5 = JOptionPane.showInputDialog("What organ helps sharks float?");
+		if (answer5.equals("liver")) {
+			score = score + 1;
+		}
+		
+		JOptionPane.showMessageDialog(null, "your final score is " + score + "/5.");
 		
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
